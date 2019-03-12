6314ae96dc491a2400505b22bfe864fd525ef59d
Boris Bershadsky <b@borisb.ca>
2019-03-12 15:40:58 -0400

    added basic mn stats

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
+require 'http'
+require 'json'
+
 class WelcomeController < ApplicationController
   def index
+    @response = HTTP.get('http://pool2.cryptopowered.club/aba-mn.txt')
+    # @result = JSON.parse(@response)
+    
+    json = JSON.parse(@response)
+    @mn_total = json['total'] # prints "bar"
+    @mn_stable = json['stable'] # prints "bar"
+    @mn_enabled = json['enabled'] # prints "bar"
   end
 end
